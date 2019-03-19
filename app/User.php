1ccd17f21f10fa790e00f5b491412447f04785df
Vikram Anand Bhushan(vikramIde) <vikrambanand.ide@gmail.com>
2019-03-20 00:36:20 +0530

    Added auth and JWT authentication

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -39,16 +39,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // Rest omitted for brevity
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
+     *
      * @return mixed
      */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
+
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
+     *
      * @return array
      */
     public function getJWTCustomClaims()
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => 'api'], function ($router) {
+    Route::group(['prefix' => 'auth'], function (){
+        Route::post('login', 'Auth\AuthController@login');
+        Route::post('register', 'Auth\AuthController@register');
+        Route::post('logout', 'Auth\AuthController@logout');
+        Route::post('refresh', 'Auth\AuthController@refresh');
+        Route::post('me', 'Auth\AuthController@me');
+    });
+});
